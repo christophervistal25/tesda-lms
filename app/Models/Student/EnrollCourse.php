e97c53483bac3a2fdc65bc8b7c4df4042ebd30c8
Christopher <christophervistal25@gmail.com>
2020-10-29 05:45:16 +0800

    Fix Postres error in no primary key

--- app/Models/Student/EnrollCourse.php
+++ app/Models/Student/EnrollCourse.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class EnrollCourse extends Model
 {
+	protected $primaryKey = null;
 
+	public $incrementing = false;
+	
 	public function student()
 	{
 		return $this->belongsTo('App\User', 'user_id', 'id');
