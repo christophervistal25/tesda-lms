a8001d8cda116309ffc7709072e4b48cef9eae96
Christopher <christophervistal25@gmail.com>
2020-11-04 19:28:50 +0800

    student login change email field to 'username or email'

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,10 +39,10 @@ input,span { font-family : "Poppins", sans-serif; }
                         <hr>
                         <div class="form-group row">
                             <div class="col-md-auto m-0">
-                                <span>Email</span>
+                                <span>Email or Username</span>
                             </div>
                             <div class="col-md-12">
-                                <input id="email" type="email" placeholder="johndoe@yahoo.com" class="p-3 border border-1 w-100 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="text"  class="p-3 border border-1 w-100" name="login" value="{{ old('login') }}" required >
                             </div>
                         </div>
                         <br>
@@ -52,11 +52,6 @@ input,span { font-family : "Poppins", sans-serif; }
                             </div>
                             <div class="col-md-12">
                                 <input id="password" type="password" class=" p-3 border border-1 w-100 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
                             </div>
                         </div>
                         <a href="{{ route('register') }}" class="text-primary">Create an account</a>
